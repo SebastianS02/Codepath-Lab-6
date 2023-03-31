69eb0003cc6e2dae36cb477381499bd07ed3606e
Sebastian Stec <sebastianstec@Sebastians-MacBook-Pro.local>
2023-03-31 15:02:20 -0400

    Added 404 page for invalid links

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Layout from '../routes/Layout';
 import DetailView from '../routes/DetailView';
 
@@ -12,6 +12,14 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route path="/" element={<Layout />}>
       <Route index={true} element={<App />} />
       <Route index={false} path="/coinDetails/:symbol" element={<DetailView />} />
+      <Route path="*" element={
+        <main>
+          <h3>Error 404: Page Not Found</h3>
+          <Link to="/">
+            Back to Home
+          </Link>
+        </main>
+       } />
       </Route>
     </Routes>
   </BrowserRouter>
